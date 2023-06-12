e13d560202969302eac2e8c02535a104749e9da7
Ismael-ux1 <ismaelabdula1@gmail..com>
2023-06-12 15:01:37 -0700

    a function that replaces an element in a list at a specific position

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    """
+  Replaces an element in a list at a specific positio.
+
+  Args:
+    my_list: The list to be modified.
+    idx: The index of the element to be replaced.
+    element: The new element to be inserted.
+
+  Returns:
+    A copy of the list with the element index replaced
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
