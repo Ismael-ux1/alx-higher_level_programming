cddbceccc959f1d297fc8edd98e2914e70ff0941
Ismael-ux1 <ismaelabdula1@gmail..com>
2023-06-14 09:18:10 -0700

    A program that  handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,29 +3,14 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     import sys
 
-    def my_calculator():
-        if len(sys.argv) != 4:
-            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-            sys.exit(1)
-
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    result = None
-
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if len(sys.argv) != 4:
+        print("Usage:./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
-    sys.exit(0)
-    my_calculator()
+    oop = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(oop.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, oop[sys.argv[2]](a, b)))
