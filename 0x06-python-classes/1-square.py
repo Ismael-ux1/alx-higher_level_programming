ead6cb55ea12bcc99be4a18a4ae6589445386f6a
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-06-27 17:29:08 -0700

    a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A class that defines a square """
+
+
+class Square:
+    """ A class that defines a square. """
+    def __init__(self, size):
+        """
+        Constructor method for Square.
+
+        Parameters:
+        - size (int): The size of the square.
+        """
+        self.__size = size
