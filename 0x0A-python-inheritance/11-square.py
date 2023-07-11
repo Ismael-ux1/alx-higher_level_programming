0130cdcd87b58bca07f69bc63287e3c5d5379696
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-07-10 19:59:34 -0700

    fix the square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -3,23 +3,15 @@
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Rectangle:
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
