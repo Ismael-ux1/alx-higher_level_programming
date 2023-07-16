45aa36f393c54c61d697abe6a3a59869aa147224
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-07-16 05:29:43 -0700

    added a test for max in the middle

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-"""
-
-This module defines a matrix division function
-
-"""
+""" This module defines a matrix division function """
 
 
 def matrix_divided(matrix, div):
+    """
+    Divide each elemnet of the matrix by divisor.
+
+    Args:
+        matrix: A matrix represented as a list of lists of intgers/floats
+        div (int or float): The divisor.
+
+    Returns:
+      list: A new matrix where each element is the result of dividingelement
+
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+         ZeroDivisionError: If div is equal to 0.
+    """
     # Check if matrix is a list of lists of integers or floats
     if not isinstance(matrix, list) or \
             not all(isinstance(row, list) for row in matrix):
