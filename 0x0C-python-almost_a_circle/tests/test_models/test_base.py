ffbb4be081e0608665e880f98b36ba09b4d4510e
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-07-21 03:38:34 -0700

    Updated the class Base by adding the class method def save_to_file(cls, list_objs):

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,7 +2,11 @@
 """ Test case for base class """
 
 import unittest
+import json
+import os
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -18,25 +22,52 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b2.id, 2)
         self.assertEqual(b3.id, 3)
 
-        def test_base_custom_id(self):
-            # Create an instance of base with a custom ID
-            b = Base(10)
+    def test_base_custom_id(self):
+        # Create an instance of base with a custom ID
+        b = Base(10)
 
-            # Check if the custom ID is assigned correctly
-            self.assertEqual(b.id, 10)
+        # Check if the custom ID is assigned correctly
+        self.assertEqual(b.id, 10)
 
-        def test_to_json_string_empty(self):
-            """ Test to_json_string with an empty list. """
-            self.assertEqual(Base.to_json_string(None), "[]")
-            self.assertEqual(Base.to_json_string([]), "[]")
+    def test_to_json_string_empty_list(self):
+        # Test to_json_string with an empty list
+        json_string = Base.to_json_string([])
+        self.assertEqual(json_string, "[]")
 
-        def test_to_json_string(self):
-            """ Test to_json_string with a non empty list of dictionaries. """
-            json_string = [{'id': 1, 'name': 'Alice'}, {'id': 2, 'name': "Bob"}]
-            json_string = Base.to_json_string(json_string)
-        self.assertEqual(type(json_string), str)
-        self.assertEqual(json_string, '[{id": 1, "name": "Alice"}, '
-                         '{"id": 2, "name": "Bob"}]')
+    def test_to_json_string_none_list(self):
+        # Test to_json_string with a None list
+        json_string = Base.to_json_string(None)
+        self.assertEqual(json_string, "[]")
+
+    def test_to_json_string(self):
+        # Test to_json_string with a list of dictionaries
+        list_dictionaries = [
+            {"id": 1, "name": "Alice"},
+            {"id": 2, "name": "Bob"}
+            ]
+        json_string = Base.to_json_string(list_dictionaries)
+        self.assertEqual(json_string, '[{"id": 1, "name": "Alice"}, {"id": 2, "name": "Bob"}]')
+
+    def test_base_save_to_file(self):
+        # Test save_to_file method with list of Rectangles
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        rectangles = [r1, r2]
+        Rectangle.save_to_file(rectangles)
+        with open("Rectangle.json", "r") as file:
+            data = json.load(file)
+            self.assertEqual(data, [r1.to_dictionary(), r2.to_dictionary()])
+        os.remove("Rectangle.json")
+
+        # Test save_to_file method with list of Squares
+        s1 = Square(5)
+        s2 = Square(9, 3, 1)
+        squares = [s1, s2]
+        Square.save_to_file(squares)
+        with open("Square.json", "r") as file:
+            data = json.load(file)
+            self.assertEqual(data, [s1.to_dictionary(), s2.to_dictionary()])
+        os.remove("Square.json")
 
 
 if __name__ == '__main__':
