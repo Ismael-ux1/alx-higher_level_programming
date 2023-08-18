8d47bf7843ec0b8090787a45cf63133813e78bb0
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-08-18 14:59:49 -0700

    updated Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- Displays the top 3 cities with the highest average temperature during July and August,
 -- ordered by temperature (descending)
-SELECT city, AVG(temperature) AS average_temperature
-FROM weather_data
-WHERE MONTH(date) IN (7, 8)
-GROUP BY city
-ORDER BY average_temperature DESC
+SELECT `city`, AVG(`value`) AS `avge_temp`
+FROM `temperatures`
+WHERE `MONTH` = 7, OR `MONTH` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
 LIMIT 3;
