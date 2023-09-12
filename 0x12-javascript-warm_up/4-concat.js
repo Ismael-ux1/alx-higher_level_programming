6ad5e3b4f8232acbcbf56a9c9c825dd2812102fd
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-11 19:08:05 -0700

    updatd the  Create a sentence script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,8 +2,7 @@
 // A script that prints two arguments passed to it, in the following format: “ is ”
 const myArgs = process.argv.slice(2);
 
-if (myArgs.length === 2) {
-  console.log(myArgs[0] + ' is ' + myArgs[1]);
-} else {
-  console.log('undefined is undefined');
-}
+const arg1 = myArgs[0] || 'undefined';
+const arg2 = myArgs[1] || 'undefined';
+
+console.log(arg1 + ' is ' + arg2);
