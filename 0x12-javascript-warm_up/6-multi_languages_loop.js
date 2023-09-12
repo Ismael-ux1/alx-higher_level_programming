6c0cdacb73af528b63a7aaca20a6679535164a96
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-11 19:30:18 -0700

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Define an array of strings containing the lines to be printed
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+// Use a for...of loop to iterate through each line in the array
+for (const line of lines) {
+  console.log(line);
+}
