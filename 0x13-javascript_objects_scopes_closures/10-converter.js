c15a544302fa581a5dc36bf40a42787292c39051
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-13 17:29:17 -0700

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  return function convertNumberToBase(num) {
+  return function convertNumberToBase (num) {
     return num.toString(base);
   };
 };
