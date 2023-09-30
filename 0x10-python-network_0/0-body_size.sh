06ac2acdf6983295c5578193b37663e07baf1c16
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-09-30 22:35:15 +0300

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # A Bash script that send a GET request to the URL and display the size of the body of the response
 
-# URL as the first argument to the script
-URL="$1"
-
-# Send a request to the URL and get the response body
-RESPONSE_BODY=$(curl -s -o /dev/null -w "%{size_download}\n" "$URL")
-
-# Display the size of the response body in bytes
-echo "$RESPONSE_BODY"
+# Send a request to the URL and display only the size of the body of,
+# the response in bytes
+curl -sI "$1" | grep 'Content-Length' | cut -d' ' -f2
