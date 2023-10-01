9950560e0b5ffc7278d65e726d0a5565e10e95df
Ismael-ux-1 <ismaelabdulahi1@gmail.com>
2023-09-30 19:44:05 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# POST request with variable in the request body, display response body
-url="$1"
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-curl -s -d "email=$email&subject=$subject" "$url"
+# POST request with variable in the request body, display response bod
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
