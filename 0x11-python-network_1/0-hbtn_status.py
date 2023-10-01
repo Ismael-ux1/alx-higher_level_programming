383c5c9294b6643c0917c2311d028ea555115a70
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-01 21:03:26 +0300

    updated what is my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import urllib.request
 
-# Use a with statement to open the url adn 
+# Use a with statement to open the url and fetch the response
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     print("Body response:")
