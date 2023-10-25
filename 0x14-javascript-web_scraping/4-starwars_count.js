b8dcedb5b5538c11d4a4387063a54e1d2959af1c
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-24 19:50:08 -0700

    updated Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,33 +1,17 @@
 #!/usr/bin/node
-// Import the request module
 const request = require('request');
-
-// Get the API URL from the first argument
 const apiUrl = process.argv[2];
-
-// Make a GET request to the API
 request(apiUrl, function (error, response, body) {
-  // Handle any errors
   if (error) {
     console.error(error);
     return;
   }
-
-  // Parse the response body as JSON
   const data = JSON.parse(body);
-
-  // Initialize a counter for the number of movies
   let count = 0;
-
-  // Loop through the results array
   for (const film of data.results) {
-    // Check if Wedge Antilles is in the characters array
     if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      // Increment the counter
       count++;
     }
   }
-
-  // Print the counter
   console.log(count);
 });
