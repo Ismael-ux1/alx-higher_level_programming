7d57e9ffb2a79613438e9b5260decbcb41f4e766
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-11-04 01:35:41 +0300

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Run this function when the DOM is ready
+document.addEventListener('DOMContentLoaded', function () {
+    // Change the header color to red
+    document.querySelector('header').style.color = '#FF0000';
+  });
