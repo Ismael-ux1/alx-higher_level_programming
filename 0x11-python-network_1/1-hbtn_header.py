10b9bc3787d806cc5005659394cdc910a9eb9199
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-04-25 13:43:36 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,21 +3,15 @@
 import urllib.request
 import sys
 
-# Check if the correct number of arguments are provided
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
 
+# Get the URL from command line areguments
 url = sys.argv[1]
 
-try:
-    # Send a GET request to the provided URL
-    with urllib.request.urlopen(url) as response:
-        # Check if the 'X-Request-Id' header is present in the response
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(f"{x_request_id}")
-        else:
-            print("X-Request-Id header not found in the response.")
-except urllib.error.URLError as e:
-    print(f"Error: {e}")
+# Send a GET request to the provided URL
+with urllib.request.urlopen(url) as response:
+    # Get the value of the X-Request-ID variable from the header,
+    # of the response
+    request_id = response.headers.get('X-Request-Id')
+
+    # Display the value of the X-Request-Id variable
+    print(request_id)
