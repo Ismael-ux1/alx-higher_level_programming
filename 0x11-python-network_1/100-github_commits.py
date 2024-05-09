77fcef518ccd632f761ea0a6c4f160d6fbec7e85
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-05-09 11:44:04 -0700

    updated #task time for interview

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+""" doc """
 import sys
 import requests
 
+
 def get_repo_info(repo_name, owner_name):
+    """ doc """
     url = f"https://api.github.com/repos/{owner_name}/{repo_name}"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         return None
+
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
